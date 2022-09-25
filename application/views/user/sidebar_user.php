9f421ede5ce4c955c1a9579d66fd932abc94fe61
lelutmaonk <putuadiputra51@gmail.com>
2022-09-25 15:47:40 +0800

    fifth commit

--- application/views/user/sidebar_user.php
+++ application/views/user/sidebar_user.php
@@ -25,40 +25,47 @@
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Administrator</div>
-                            <a class="nav-link" href="index.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
-                            <div class="sb-sidenav-menu-heading">User</div>
-                            <a class="nav-link" href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
-                                My Profile
-                            </a>
-                            <a class="nav-link" href="charts.html">
+                            <!-- QUERY MENU -->
+                            <?php
+                                $roleId = $this->session->userdata('role_id');
+                                $queryMenu = "SELECT `t_user_menu`.`id`, `menu`
+                                                FROM `t_user_menu` JOIN `t_user_access_menu`
+                                                ON `t_user_menu`.`id` = `t_user_access_menu`.`menu_id`
+                                                WHERE `t_user_access_menu`.`role_id` = $roleId
+                                                ORDER BY `t_user_access_menu`.`menu_id` ASC";
+                                $menu = $this->db->query($queryMenu)->result_array();
+                            ?>
+
+                            <!-- Looping Menu -->
+                            <?php foreach($menu as $m) : ?>
+                                <div class="sb-sidenav-menu-heading">
+                                    <?php echo $m['menu']?>
+                                </div>
+
+                                <!-- query sub menu -->
+                                <?php
+                                    $menuId = $m['id'];
+                                    $querySubMenu = "SELECT * FROM `t_user_sub_menu`
+                                                        WHERE `menu_id` = $menuId
+                                                        AND `is_active` = 1";
+                                    $subMenu = $this->db->query($querySubMenu)->result_array();
+                                ?>
+
+                                <!-- looping sub menu -->
+                                <?php foreach($subMenu as $sm) : ?>
+                                    <a class="nav-link" href="<?php echo base_url($sm['url'])?>">
+                                        <div class="sb-nav-link-icon"><i class="<?php echo $sm['icon']?>"></i></div>
+                                        <?php echo $sm['title']?>
+                                    </a>
+                                <?php endforeach;?>
+
+                            <?php endforeach; ?>
+
+                            <a class="nav-link" href="<?php echo base_url('auth/logout')?>">
                                 <div class="sb-nav-link-icon"><i class="fas fa-right-from-bracket"></i></div>
                                 Logout
                             </a>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts
-                            </a>
-                            <a class="nav-link" href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
-                            </a>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Layouts
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                                </nav>
-                            </div>
+
                         </div>
                     </div>
                 </nav>
